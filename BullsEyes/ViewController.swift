4c7168a6bc69d586bf6dfc90bc1c665d5d865bc7
Luis Avila <luis@luiss-mbp.home>
2017-11-02 09:53:55 -0400

    the slider value is correct

--- BullsEyes/ViewController.swift
+++ BullsEyes/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue : Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func sliderMover(_ slider: UISlider){
+        print("The Value of the Slider is \(slider.value)")
+        currentValue = lround(Double(slider.value))
+        
+    }
+    @IBAction func showAlert(){
+        let message = "The Value of the Slider is \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello, Comics Fans", message: message, preferredStyle: .alert)
 
+        let action = UIAlertAction(title: "Marvel", style: .default, handler: nil)
+        
+        alert.addAction(action)
 
+        present(alert, animated: true, completion: nil)
 }
+
+   
+  
+}
+
 
