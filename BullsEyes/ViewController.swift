9103d0c402e06cfdbe138c0482662f1cd59941b9
Luis Avila <luis@luiss-mbp.home>
2017-11-06 19:22:35 -0500

    28. Section 3: Challenge: Adding Polish Get some final practice with coding basics by giving the user bonus points if they are very close to the target

--- BullsEyes/ViewController.swift
+++ BullsEyes/ViewController.swift
@@ -56,21 +56,25 @@ class ViewController: UIViewController {
     @IBAction func showAlert() {
         
         let difference = abs(targetValue - currentValue)
-        let points = 100 - difference
+        var points = 100 - difference
         
-        score += points
         
         let title: String
         if difference == 0 {
             title = "Perfect!"
+            points += 100
         }else if difference < 5 {
             title = "You almost had it!"
+            if difference == 1{
+                points += 50
+            }
         }else if difference < 10{
             title = "Pretty good!"
         }else{
             title = "Not even close..."
         }
-        
+        score += points
+
         let message = "You scored \(points) points"
         
         let alert = UIAlertController(title: title , message: message, preferredStyle: .alert)
